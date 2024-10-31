0e46362c5711a561755e3277524a1dc707dade5d
Linhan Li <h1nlee@163.com>
2024-10-31 18:52:12 +0800

    Update launch.py
    删除多余代码

--- launch.py
+++ launch.py
@@ -3,40 +3,18 @@ import os
 import time
 import win32gui, win32api, win32con
 
-user='   ' # 填入你的qmt用户名
-password='   ' # 填入你的qmt密码
-cmdstr="start D:\\国金证券QMT交易端111\\bin.x64\\XtItClient.exe "# 填入你的qmt路径
+# 适用于国金qmt，登录需要时间，如自动运行多个qmt请间隔足够长时间
 
-#关闭qmt         
-#try:
-#    os.system('taskkill /F /t /im XtItClient.exe')
-#except Exception as e:
-#    print(e)
-#time.sleep(10)
+user='   '                                                             # 填入您的qmt用户名
+password='   '                                                         # 填入您的qmt密码
+cmdstr="start D:\\国金证券QMT交易端111\\bin.x64\\XtItClient.exe "       # 填入您的qmt路径
 
-#运行qmt
-try:
-    os.system(cmdstr)
-except Exception as e:
-    print(e)
-
-time.sleep(10)
 
 def input_content(hwd, content):
   mylist=[int(ord(item)) for item in str(content)]
   for item in mylist:
       click_keys(hwd,item)
   time.sleep(0.1)
-
-def get_my_child_window(parent):
-    hwndChildList = []
-    win32gui.EnumChildWindows(
-        parent, lambda hwnd, param: param.append((hwnd,win32gui.GetWindowText(hwnd),win32gui.GetClassName(hwnd),win32gui.GetWindowRect(hwnd))),  hwndChildList)
-    for i in range(len(hwndChildList)):
-        item=hwndChildList[i]
-        #print item[0],item[1],item[2],i
-    return hwndChildList
-
 
 def find_child_window(parent_handle,winstr,classname=""):
     result=[]                
@@ -53,17 +31,18 @@ def find_child_window(parent_handle,winstr,classname=""):
 def click_keys(hwd, mykey):
   win32api.SendMessage(hwd, win32con.WM_KEYDOWN, mykey, 0)
   win32api.SendMessage(hwd, win32con.WM_KEYUP, mykey, 0)
-  
-#获取qmt句柄
+
+# 进入qmt文件夹
+os.system(cmdstr)
+# 获取qmt句柄
 qmt_handle=find_child_window(0,u"国金证券QMT交易端","Qt5QWindowIcon")[0]
-#qmt_handle=win32gui.FindWindow("Qt5QWindowIcon",u"国金证券QMT交易端 1.0.0.29456")
 time.sleep(2)
-#后台输入账号
+# 后台输入账号
 input_content(qmt_handle,str(user))
 time.sleep(2)
 click_keys(qmt_handle,win32con.VK_RETURN)
 time.sleep(2)
-#后台输入密码
+# 后台输入密码
 input_content(qmt_handle,str(password))
 time.sleep(2)
 click_keys(qmt_handle,win32con.VK_RETURN)
