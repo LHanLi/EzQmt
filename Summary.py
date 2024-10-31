7ac4f31876031034ac22b022dcd85d2e9dd74fac
Linhan Li <h1nlee@163.com>
2024-10-31 18:24:03 +0800

    Update Summary.py
    每日总结账户资产、挂单、成交和分策略持仓情况

--- Summary.py
+++ Summary.py
@@ -3,16 +3,15 @@ import datetime, re, os, time
 import numpy as np
 import pandas as pd
 
-# 16:20总结当日交易
-# 当日输出账户、持仓、交割、委托单
+# summary_time(16:20)总结当日交易
+# 当日输出账户资产现金、持仓、交割、委托单以及各策略收盘持仓情况
 
-
-ACCOUNT = '55012783'       # 259800
+ACCOUNT = '0000'                                                   # 填写您的账号
 account_type = 'STOCK'
-
 strategy_name = 'summary'
 # 日志文件
-save_loc = 'D:/cloud/XtQuant/summary-' + ACCOUNT + '/' + account_type + '/'
+save_loc = 'D:/cloud/monitor/QMT/summary/'                         # 填写您的结算文件（用于post分析及复杂多策略运行）保存位置
+save_loc = save_loc + ACCOUNT + '/' + account_type + '/'    
 summary_time = '162000'
 
 #######################################################################################################
@@ -26,59 +25,23 @@ summary_time = '162000'
 # log 函数
 def log(*txt):
     try:
-        f = open(logfile,'a+', encoding='gbk')
-        write_str = ('\n'+' '*35).join([str(i) for i in txt])
-        f.write('%s,        %s\n' % \
-            (datetime.datetime.now(), write_str))
+        f = open(logfile,'a+', encoding='gbk')  
+        f.write('%s'%datetime.datetime.now()+' '*6)  # 时间戳
+        if type(txt[0])==pd.Series:
+            f.write('name: %s\n'%txt[0].name)
+            for i,v in txt[0].items():
+                f.write(' '*32+str(i)+', '+str(v)+'\n')
+        elif type(txt[0])==pd.DataFrame:
+            f.write(' '.join([str(i) for i in txt[0].columns])+'\n')
+            for i,r in txt[0].iterrows():
+                f.write(' '*29+str(i)+': ')
+                f.write(', '.join([str(j) for j in r.values])+'\n')
+        else:
+            write_str = ('\n'+' '*32).join([str(i) for i in txt])
+            f.write('%s\n' %write_str)
         f.close()
     except PermissionError as e:
         print(f"Error: {e}. You don't have permission to access the specified file.")
-
-# log ser函数
-def log_ser(text, write_time=True):
-    with open(logfile, 'a') as f:
-        if type(text)==type(pd.Series()):
-            for i,v in text.items():
-                f.write(str(i)+','+str(v))
-                f.write('\n')
-        else:
-            f.write("unable to identify message type\n")
-
-########################################### 行情数据 ###################################################
-
-# 获取行情快照数据 DataFrame, index:code 
-SHmul = 10
-SZmul = 10
-def get_snapshot(C, code_list):
-    # 获取标的快照数据
-    df = C.get_full_tick(code_list)
-    df = pd.DataFrame.from_dict(df, dtype='float').T.reset_index().rename(columns={'index': 'code'})
-    # 盘口
-    bidPrice_columns = ['bidp1','bidp2','bidp3','bidp4','bidp5']
-    askPrice_columns = ['askp1','askp2','askp3','askp4','askp5']
-    df[bidPrice_columns] = df['bidPrice'].apply(pd.Series, index=bidPrice_columns)
-    df[askPrice_columns] = df['askPrice'].apply(pd.Series, index=askPrice_columns)
-    bidVol_columns = ['bidv1','bidv2','bidv3','bidv4','bidv5']
-    askVol_columns = ['askv1','askv2','askv3','askv4','askv5']
-    df[bidVol_columns] = df['bidVol'].apply(pd.Series, index=bidVol_columns)
-    df[askVol_columns] = df['askVol'].apply(pd.Series, index=askVol_columns)
-    # 中间价
-    df['mid'] = (df['bidp1'] + df['askp1'])/2
-    # 涨跌停则bid/askprice为0
-    df.loc[(df['bidp1'] == 0) | (df['askp1'] == 0),'mid'] = df['bidp1'] + df['askp1'] # 涨跌停修正
-    ## 展示列 最新价，当日成交额、成交量(手）、最高价、最低价、开盘价 
-    # 盘口 askp\askv*/bid* 买卖5档， 昨收
-    ## 中间价 askp\askv*/bid* 买卖5档，需要使用券商行情
-    display_columns = ['code', 'lastPrice', 'amount', 'volume', 'high', 'low', 'open', 'lastClose',\
-        'mid', 'askp1', 'askp2', 'askp3', 'askp4', 'askp5', \
-            'bidp1', 'bidp2', 'bidp3', 'bidp4', 'bidp5', \
-            'askv1', 'askv2', 'askv3', 'askv4', 'askv5',\
-              'bidv1', 'bidv2', 'bidv3', 'bidv4', 'bidv5']
-    df = df[display_columns].rename(columns={'volume':'vol'})
-    df = df.set_index('code')
-    # 有时，沪市转债单位是手，沪市需要乘一个沪市转化因子
-    df['vol'] = df['vol']*df.index.map(lambda x: SHmul if 'SH' in x else SZmul if 'SZ' in x else 1)
-    return df
 
 ########################################### 账户状态 ###################################################
 
@@ -169,57 +132,6 @@ def get_deal():
     return deal[['id', 'order_id', 'code', 'date', 'deal_time',\
         'trade_type', 'price', 'vol', 'amount', 'remark']]
 
-########################################### 买卖挂单 ###################################################
-
-# 撤单 超过wait_dur s的订单取消
-def cancel_order(C, wait_dur):
-    order = get_order()
-    # 全部可撤订单
-    order = order[order['status'].map(lambda x:(x!=53)&(x!=54)&(x!=56)&(x!=57))].copy()
-    if not order.empty:
-        # 超过等待时间撤单 insert_time 为1900年
-        order['sub_time'] = order['sub_time'].map(lambda x: datetime.datetime.strptime(x, "%H%M%S"))
-        order = order[order['sub_time'].map(lambda x: (datetime.datetime.now()-x).seconds>wait_dur)]
-        for orderid in order.index:
-            cancel(orderid, ACCOUNT, account_type, C)
-# 撤单 属于策略strat的挂单价超过最新价(1+r)或低于最新价(1-r)的订单取消
-def cancel_order_price(C, r, stratname=None):
-    order = get_order()
-    # 全部可撤订单
-    order = order[order['status'].map(lambda x:(x!=53)&(x!=54)&(x!=56)&(x!=57))].copy()
-    # 属于该策略
-    if stratname!=None:
-        order = order[order['remark']==stratname].copy()
-    if not order.empty:
-        # 最新价格
-        codes = list(set(order['code']))
-        snapshot = get_snapshot(C, codes)
-        lastPrice = snapshot[['lastPrice', 'lastClose']].apply(lambda x: \
-            x['lastPrice'] if x['lastPrice']!=0 else x['lastClose'], axis=1)
-        lastPrice = order['code'].map(lambda x: lastPrice[x])
-        if not order.empty:
-            delta = abs((order['price']-lastPrice)/lastPrice)
-            delta = delta[delta>r]
-            for orderid in delta.index:
-                cancel(orderid, ACCOUNT, account_type, C)
-#strategy_name = 'craft'
-#multiples = 10
-#卖出 
-def sell(C, code, price, vol, strategyName=strategy_name, remark=strategy_name):
-    vol = int((vol//multiples)*multiples)
-    # 卖出，单标的，账号， 代码，限价单，价格，量，策略名，立即触发下单，备注
-    if account_type=='STOCK':
-        passorder(24, 1101, ACCOUNT, code, 11, price, vol, strategyName, 2, remark, C) # 下单
-    elif account_type=='CREDIT':
-        passorder(34, 1101, ACCOUNT, code, 11, price, vol, strategyName, 2, remark, C) # 下单
-#买入
-def buy(C, code, price, vol, strategyName=strategy_name, remark=strategy_name):
-    vol = int((vol//multiples)*multiples)
-    if account_type=='STOCK':
-        passorder(23, 1101, ACCOUNT, code, 11, price, vol, strategyName, 2, remark, C) # 下单
-    elif account_type=='CREDIT':
-        passorder(33, 1101, ACCOUNT, code, 11, price, vol, strategyName, 2, remark, C) # 下单
-
 ########################################### 其他 ###################################################
 
 # 存储全局变量
@@ -255,7 +167,7 @@ def summary(C):
         # 当日买入之前持仓，对于当日成交部分，如果当日卖出则之前为对应策略持仓，如果当日买入则之前不持仓
         firststratpos = pos
         firststratpos['strat'] = 'craft'
-        firststratpos = firststratpos.set_index(['strat', 'code'])['vol']
+        firststratpos = firststratpos.reset_index().set_index(['strat', 'code'])['vol']
         deal_ = deal.rename(columns={'remark':'strat'}).copy()
         deal_['vol'] = -deal_['vol']*deal['trade_type'].map(lambda x: 1 if x==48 \
                         else -1) # >0为卖出，<0为买入 
@@ -264,7 +176,7 @@ def summary(C):
                                 else 'craft', axis=1)
         summarydeal = summarydeal.set_index(['strat', 'code'])['vol']
         firststratpos = firststratpos.add(summarydeal, fill_value=0)
-        prestratpos = firststratpos[firststratpos>0].copy()
+        prestratpos = firststratpos[firststratpos>0].reset_index().set_index(['strat', 'code'])[['vol']]
     # 当日成交汇总
     deal_ = deal.rename(columns={'remark':'strat'}).copy()
     deal_['strat'] = deal_['strat'].replace('', 'craft')
@@ -289,7 +201,7 @@ def summary(C):
                 continue
     todaystratpos = todaystratpos[todaystratpos>0].copy()
     todaystratpos.reset_index().to_csv(save_loc+'/stratpos-'+today+'.csv', index=False)
-
+    log('summary success')
 
 
 # 初始化函数 主程序
