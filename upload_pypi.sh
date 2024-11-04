6157e88c4659c355dc901330492f13117a61be09
Linhan Li <h1nlee@163.com>
2024-11-04 11:54:47 +0800

    Create upload_pypi.sh

---
+++ upload_pypi.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+mypython=/Users/h1nlee/miniconda3/bin/python
+
+# Upload project to pypi
+
+rm -rf ./build
+rm -rf ./dist
+rm -rf ./EzQmt.egg-info
+
+$mypython setup.py sdist bdist_wheel
+twine check dist/*
+twine upload dist/*
+# (-u __token__ -p password)
